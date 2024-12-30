4ee6bfa85324248f65140aa09247e5bcb2726c2a
Osuji <weezdomosuji@gmail.com>
2024-12-30 09:46:12 +0100

    added effects on hover

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const panels = document.querySelectorAll('.panel');
+panels[0].classList.add('active');
 
-panels.forEach((panel) => {
-  panel.addEventListener('click', () => {
+panels.forEach((panel, index) => {
+  panel.addEventListener('mouseover', (event) => {
+    if (event.target === panel) {
+      removeActiveClasses();
+      panel.classList.add('active');
+    }
+  });
+  panel.addEventListener('mouseout', () => {
     removeActiveClasses();
-    panel.classList.add('active');
+    panels[0].classList.add('active');
   });
 });
 
